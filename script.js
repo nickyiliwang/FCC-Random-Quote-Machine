7c7cc3e7a248c1b3ffe59cbe75653821142a6f64
Nick Wang <nickyiliwang@gmail.com>
2019-04-24 22:36:31 -0400

    Added html css and js files

---
+++ script.js
@@ -0,0 +1,27 @@
+var x = document.getElementById("veigar-audio");
+
+onStart();
+
+function onStart() {
+    generateRandomColors();
+    generateRandomQuote()
+}
+
+
+
+
+
+function playAudio() { 
+  x.play(); 
+} 
+
+
+
+function generateRandomColors() {
+
+    
+}
+
+function generateRandomQuote(){
+
+}
