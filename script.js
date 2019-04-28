78bceead19ce368c7e38647a31eb68016a4754d2
Nick Wang <nickyiliwang@gmail.com>
2019-04-28 11:43:06 -0400

    updated html, logic and buttons

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var x = document.getElementById("veigar-audio");
+var quotesArr = ["I will swallow your soul.","What's black and blue and is about to show you the definition of pain?!", "It's only a short way? Is that a short joke?!"]
+var newQuote = document.querySelector("#new-quote");
+var quote = document.querySelector("#text");
+
+
 
 onStart();
 
 function onStart() {
+  newQuote.addEventListener('click', function name(params) {
+  
+    quote.textContent = quotesArr[getRandomInt(2)];
+  
+  
+  });
+  
     generateRandomColors();
     generateRandomQuote()
 }
@@ -16,12 +28,12 @@ function playAudio() {
 } 
 
 
-
 function generateRandomColors() {
 
     
 }
 
-function generateRandomQuote(){
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
+
